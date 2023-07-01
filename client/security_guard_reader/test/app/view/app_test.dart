42cbf556df43a961ff2f7cd4fb2e26e946c79ee8
AndreXi <andrexilol@gmail.com>
2023-07-01 01:28:29 -0400

    remove: unused

--- client/security_guard_reader/test/app/view/app_test.dart
+++ client/security_guard_reader/test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:security_guard_reader/app/app.dart';
-import 'package:security_guard_reader/counter/counter.dart';
+import 'package:security_guard_reader/home/view/home_page.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(HomePage), findsOneWidget);
     });
   });
 }
