fb2e66aed9bbba32159e3b38e9d0bbff72532df7
AndreXi <andrexilol@gmail.com>
2023-07-01 01:40:20 -0400

    fix: app test

--- client/security_guard_reader/test/app/view/app_test.dart
+++ client/security_guard_reader/test/app/view/app_test.dart
@@ -1,12 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:security_guard_reader/app/app.dart';
-import 'package:security_guard_reader/home/view/home_page.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders MaterialApp', (tester) async {
       await tester.pumpWidget(App());
-      expect(find.byType(HomePage), findsOneWidget);
+      expect(find.byType(MaterialApp), findsOneWidget);
     });
   });
 }
